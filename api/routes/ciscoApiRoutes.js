6695cd2406b7f06868d2fc566009daf86244f56a
Varun Iyer <dev.varuniyer@gmail.com>
2017-10-04 04:32:29 -0400

    Debug routes

--- api/routes/ciscoApiRoutes.js
+++ api/routes/ciscoApiRoutes.js
@@ -3,5 +3,5 @@ module.exports = function(app) {
 	var content = require('../controllers/ciscoApiController');
 
 	app.route('/content').get(content.list_all_content).post(content.create_content);
-	app.route('/content:contentId').get(content.read_content).put(content.update_content).delete(content.delete_content);
+	app.route('/content/:contentId').get(content.read_content).put(content.update_content).delete(content.delete_content);
 }
